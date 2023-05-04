3a3b6092e466e18d2d37d13b912c4969f550d6ef
Yaseen Al Hasan <ya705668@gmail.com>
2023-05-04 08:03:24 -0700

    complete E-commerce project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// js for home
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar')
@@ -12,3 +14,25 @@ if (close) {
         nav.classList.remove('active');
      })
 };
+
+
+// js for sprodect
+
+var MainImg = document.getElementById('MainImg');
+var smallimg = document.getElementsByClassName('small-img');
+
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
+
+
